b067e395f86f51a4b479b98f8c3a5d8c919f676f
chaty141 <chat1419@mymail.unisa.edu.au>
2025-11-14 18:11:42 +1030

    Adding a percent text to display the volume to the user, the text was orginally just added on to the bottom but then it overwrote itself, so then I added remove all the graph would redraw. Now the text updates but the graph does not

--- src/components/D3.js
+++ src/components/D3.js
@@ -5,12 +5,15 @@ import * as d3 from 'd3';
 
 const D3VolumeChart = ({ data }) => {
     const svgRef = useRef();
+    const initialised = useRef(false);
+
+    
 
     useEffect(() => {
         const svg = d3.select(svgRef.current);
 
         const width = 350;
-        const height = 400;
+        const height = 350;
         const margin = { top: 20, right: 20, bottom: 40, left: 10 };
 
         const innerWidth = width - margin.left - margin.right;
@@ -27,30 +30,47 @@ const D3VolumeChart = ({ data }) => {
             .range([0, innerHeight])
             .padding(0.2);
 
-        svg.selectAll('.bar')
-            .data(data)
-            .join(
-                enter => enter.append('rect')
-                    .attr('class', 'bar')
-                    .attr('x', margin.left)
-                    .attr('y', d => yScale(d.label) + margin.top)
-                    .attr('width', 0)
-                    .attr('height', yScale.bandwidth())
-                    .attr('fill', 'steelblue')
-                    .call(enter => enter.transition()
+
+        if (!initialised.current) {
+            initialised.current = true;
+
+            svg.selectAll('.bar')
+                .data(data)
+                .join(
+                    enter => enter.append('rect')
+                        .attr('class', 'bar')
+                        .attr('x', margin.left)
+                        .attr('y', d => yScale(d.label) + margin.top)
+                        .attr('width', 0)
+                        .attr('height', yScale.bandwidth())
+                        .attr('fill', 'steelblue')
+                        .call(enter => enter.transition()
+                            //.duration(750)
+                            .attr('width', d => xScale(d.value))
+                        ),
+                    update => update.call(update => update.transition()
                         //.duration(750)
                         .attr('width', d => xScale(d.value))
-                    ),
-                update => update.call(update => update.transition()
-                    //.duration(750)
-                    .attr('width', d => xScale(d.value))
-                )
-            );
+                    )
+                );
+
+            svg.append("text")
+                .attr("class", "percent-text")
+                .attr("x", width / 2)
+                .attr("y", 60)
+                .attr("text-anchor", "middle")
+                .attr("font-size", "16px")
+        }
+
+
+        const volumeVal = data[0].value;
+        const percent = Math.round(volumeVal * 100);
+        svg.select(".percent-text").text(`${percent}%`);
 
     }, [data]);
 
     return (
-        <svg ref={svgRef} width={380} height={400}></svg>
+        <svg ref={svgRef} width={380} height={250}></svg>
     )
 }
 
